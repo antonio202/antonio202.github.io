82573a0d4470ae97b2cfb39cae79e11994b9615b
antonio202 <46777518+antonio202@users.noreply.github.com>
2023-03-13 17:12:49 +0100

    Sección equipamiento

---
+++ js/general.js
@@ -0,0 +1,11 @@
+function irAPalas(ruta){
+    window.location.href = '../palas/'+ruta+'.html';
+}
+
+function irAPagina(nombre){
+    if(nombre==='zapatillas'){
+        window.location.href = '../equipamiento/zapatillas.html';
+    }else{
+        window.location.href = '../equipamiento/accesorios.html';
+    }
+}
