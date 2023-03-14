a583b4d06d7349b118786f0a568922499e18cfe2
antonio202 <46777518+antonio202@users.noreply.github.com>
2023-03-14 20:05:49 +0100

    Comentario y puntuación acabados

--- js/general.js
+++ js/general.js
@@ -21,6 +21,7 @@ function irAAccesorios(accesorio){
 function cajaComentarios(){
 	var name=document.getElementById('name').value;
 	var comment=document.getElementById('comment').value;
+	var puntuacion=document.getElementById('puntuacion').textContent;
  
 	if(name =="" || comment ==""){
 		alert("Introduzca el nombre y el comentario.");
@@ -28,20 +29,32 @@ function cajaComentarios(){
 		var divp=document.createElement('div');
 		var element_name=document.createElement('h5');
 		var element_message=document.createElement('p');
+		var element_puntuacion=document.createElement('p');
 		var element_line=document.createElement('hr');
 		var txt_name=document.createTextNode(name);
 		var txt_message=document.createTextNode(comment);
+		var txt_puntuacion=document.createTextNode('Nota: '+puntuacion);
 		element_name.appendChild(txt_name);
 		element_message.appendChild(txt_message);
+		element_puntuacion.appendChild(txt_puntuacion);
 		element_line.style.border='1px solid #000';
 		divp.appendChild(element_name);
 		divp.appendChild(element_line);
 		divp.appendChild(element_message);
+		divp.appendChild(element_puntuacion);
 		divp.setAttribute('class', 'rounded bg-primary p-3 m-3');
 		document.getElementById('contenedorComentarios').appendChild(divp);
- 
 		document.getElementById('name').value="";
 		document.getElementById('comment').value="";
 	}
  
+}
+
+function mostrarPuntuacion(puntuacion){
+	var element_puntuacion = document.getElementById('puntuacion');
+	if(element_puntuacion.textContent!=null){
+		element_puntuacion.textContent="";
+	}
+	var txt_puntuacion = document.createTextNode(puntuacion);
+	element_puntuacion.appendChild(txt_puntuacion);
 }
